0669f2d62c40013040dae341113c3c30851226b5
aniketbhabat <142041372+aniketbhabat@users.noreply.github.com>
2024-03-03 22:52:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-const btnE = document.getElementById("btn");
-const birthdayE = document.getElementById("birthday");
-const resultE = document.getElementById("result");
+const btnEl = document.getElementById("btn");
+const birthdayEl = document.getElementById("birthday");
+const resultEl = document.getElementById("result");
 
-function calculateAge(){
-    const birthdayValue = birthdayE.ariaValueMax;
-    if (birthdayValue === ""){
-        alert("Please enter your Bithday");
-    }else{
-        const age = getAge{(birthdayValue);
-            resultE.innerHTML = `Your age is ${age} ${age>1 ? "years" : "Year"}old`;
-        }
-    }
+function calculateAge() {
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("Please enter your birthday");
+  } else {
+    const age = getAge(birthdayValue);
+    resultEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
+  }
 }
 
 function getAge(birthdayValue) {
-        const currentDate = new Date();
-        const birthdayDate = new Date(birthdayValue);
-        let age = currentDate.getFullYear() - birthdayDate.getFullYear();
-        const month = currentDate.getMonth() - birthdayDate.getMonth();
-      
-        if (
-          month < 0 ||
-          (month === 0 && currentDate.getDate() < birthdayDate.getDate())
-        ) {
-          age--;
-        }
-      
-        return age;
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
 }
-      
+
 btnEl.addEventListener("click", calculateAge);
-      
